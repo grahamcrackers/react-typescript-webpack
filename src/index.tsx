2b0262ae6229b7f90ea8010516894e5bc74d1a24
Graham Rogers <gmrogers04@gmail.com>
2018-09-27 22:05:46 -0500

    it works

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+//import { AppContainer } from 'react-hot-loader';
+//import configureStore, { history } from './store/configureStore';
+//import Root from './components/Root';
 import { Hello } from "./components/Hello";
+
+//const store = configureStore();
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
+    document.getElementById("app")
 );
+
+// render(
+//   <AppContainer>
+//     <Root store={store} history={history} />
+//   </AppContainer>,
+//   document.getElementById('app')
+// );
+
+// if (module.hot) {
+//   module.hot.accept('./components/Root', () => {
+//     const NewRoot = require('./components/Root').default;
+//     render(
+//       <AppContainer>
+//         <NewRoot store={store} history={history} />
+//       </AppContainer>,
+//       document.getElementById('app')
+//     );
+//   });
+// }
---
+++ webpack.config.dev.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+
+export default {
+    target: 'web',
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Note: Physical files are only output by the production build task `npm run build`.
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: 'source-map', 
+    //devtool: 'cheap-module-eval-source-map', // more info:https://webpack.js.org/guides/development/#using-source-maps and https://webpack.js.org/configuration/devtool/
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     react: 'React',
+    //     'react-dom': 'ReactDOM'
+    // }
+};
