a0b67eebf72d03f313f6cbd02c035b6f3b6c6682
Graham Rogers <graham.rogers@stjude.org>
2018-09-29 13:34:57 -0500

    good starting point to start at

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,21 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-//import configureStore, { history } from './store/configureStore';
 import Root from './components/Root';
 import NewRoot from './components/Root';
-//import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import './styles/styles.scss';
 //require('./favicon.ico'); // Tell webpack to load favicon.ico
-//const store = configureStore();
 
-ReactDOM.render(
-    <Root />,
-    document.getElementById('app')
-);
+
+ReactDOM.render(<Root />, document.getElementById('app'));
 
 if ((module as any).hot) {
     (module as any).hot.accept('./components/Root', () => {
         ReactDOM.render(
-        <AppContainer>
-          <NewRoot />
-        </AppContainer>,
-        document.getElementById('app')
-      );
+            <AppContainer>
+                <NewRoot />
+            </AppContainer>,
+            document.getElementById('app')
+        );
     });
-  }
+}
--- webpack.config.dev.ts
+++ webpack.config.dev.ts
@@ -1,7 +1,8 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const webpackOptions = {
+const config: webpack.Configuration = {
     target: 'web',
     mode: 'development',
     entry: [
@@ -19,21 +20,66 @@ const webpackOptions = {
         extensions: ['.ts', '.tsx', '.js', '.json']
     },
     // Enable sourcemaps for debugging webpack's output.
-    devtool: 'source-map', 
+    devtool: 'source-map',
     //devtool: 'cheap-module-eval-source-map', // more info:https://webpack.js.org/guides/development/#using-source-maps and https://webpack.js.org/configuration/devtool/
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            // Create HTML file that includes references to bundled CSS and JS.
+            template: 'src/index.ejs',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            inject: true
+        })
     ],
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' }
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            },
+            {
+                test: /(\.css|\.scss|\.sass)$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [require('autoprefixer')],
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: [
+                                path.resolve(__dirname, 'src', 'scss')
+                            ],
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
         ]
-    },
+    }
 
+    // GMR: Save this for prod
     // When importing a module whose path matches one of the following, just
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
@@ -44,4 +90,4 @@ const webpackOptions = {
     // }
 };
 
-export default webpackOptions;
+export default config;
